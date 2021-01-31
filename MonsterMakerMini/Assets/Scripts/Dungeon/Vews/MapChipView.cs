5a8f1e07c6de91bc4625a7d5e20d108bad348dba
ダニエル\mastermonk <mastermonk@studioclips.net>
2021-01-31 23:47:36 +0900

    ダンジョンのパーツ追加

---
+++ MonsterMakerMini/Assets/Scripts/Dungeon/Vews/MapChipView.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapChipView : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> mapChipLists = new List<Sprite>();
+
+    [SerializeField] private GameObject mapBack = null;
+
+    private Sprite mapBackSprite = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
